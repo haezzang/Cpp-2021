58ad998644fca757a9aa52b8cac241e635c874ef
CHO HAE JEONG <67359960+haezzang@users.noreply.github.com>
2021-06-18 23:22:29 +0900

    중요부분 21.06.18

--- Class/Student3.cpp
+++ Class/Student3.cpp
@@ -5,23 +5,97 @@ class Student {
 
 public :
 	int nNo;
-	char szName[20];
+	//char szName[20];
+	char* szName;
 	Student() {
 		cout << "생성" << endl;
 		nNo = 0;
-		szName[0] = NULL;
+		//szName[0] = NULL;
+		szName = NULL;
+	}
+	Student(Student &s) :nNo(s.nNo){
+		cout << "생성" << endl;
+		//this->nNo = s.nNo;
+		szName = new char[strlen(s.szName) + 1];
+		strcpy(this->szName, s.szName);
+		cout << "생성끝" << endl;
+
 	}
 	Student(int nNo, const char* pName) {
 		this->nNo=nNo;
+		//strcpy(szName, pName);
+		szName = new char[strlen(pName) + 1];
+		cout << " 이름 생성"<<endl;
 		strcpy(szName, pName);
+	
 	}
 	~Student() {
 		cout << nNo << " 해제 되었습니다" << endl;
+		delete[] szName;
+		cout << " 이름 소멸"<<endl;
+	}
+	void prinStudent() {
+		if(szName)
+		cout << "번호 : "<< nNo <<" 이름 : "<< szName<<endl;
+	}
+	Student &copy(Student &s) {
+	
+		if (this != &s) {
+			this->nNo = s.nNo;
+			if (this->szName != NULL) {
+				delete[] szName;
+				cout << " 이름 소멸" << endl;
+			}
+				this->szName = new char[strlen(s.szName) + 1];
+				cout << " 이름 생성" << endl;
+				strcpy(this->szName, s.szName);
+			
+		}
+	
+		return *this;
+		
+	}
+
+	Student &operator=(Student &s) {
+		
+		return copy(s);
 	}
 };
 
 int main() {
-	Student a(1, "강하나");
+	Student a(1, "홍하나");
+	Student b;
+	a.prinStudent();
+	b.prinStudent();
+	b = a;
+	strcpy(a.szName, "홍길동");
+	strcpy(b.szName, "홍길동");
+	a.prinStudent();
+	b.prinStudent();
+	//b.operator=(a);
+	//b.copy(a);
+	a=a;
+	a.prinStudent();
+	Student c(2, "장발장");
+	c = a;
+	a.prinStudent();
+	c.prinStudent();
+	Student d;
+	Student e;
+	d = e = a;
+	d.prinStudent();
+	e.prinStudent();
+	cout << "해정큐티" << endl;
+	Student f = a;
+	cout << "해정프리티" << endl;
+	f.prinStudent();
+	a.prinStudent();
+
+
+	
+	//a.prinStudent();
+	//b.prinStudent();
+	/*Student a(1, "강하나");
 	Student* p = new Student(2, "홍하나");
 	delete p;
 	Student stu[10];
@@ -45,7 +119,7 @@ int main() {
 	stu1[2].nNo = 8;
 	strcpy((stu1 + 3)->szName, "백설공주");
 
-	delete[] stu1;
+	delete[] stu1;*/
 
 	return 0;
 }
